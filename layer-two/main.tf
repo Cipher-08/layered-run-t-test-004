cdda96d1c40098a6d0255ecad793f30c7f7c692f
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-11-03 19:58:05 +0530

    Update main.tf

--- layer-two/main.tf
+++ layer-two/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "prod-bucket-73283278"
+    key            = "layer-one/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
